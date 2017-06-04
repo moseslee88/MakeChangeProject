651a05b68a4cc9d7d60df19623bd407a055bf193
Moses Lee <moseslee88@gmail.com>
2017-06-04 17:27:00 -0600

    added a Method to perform when amount tendered is greater than prie of item

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ This is my week 1 homework Project
 
 I am learning how to utilize Java expressions.
 
-Note to self: Fill out this README.markdown file as a blog, talking through what i do for MakeChange.)
+(Note to self: Fill out this README.markdown file as a blog, talking through what i do for MakeChange.)
 
 The modulo operator is awesome!
--- src/libraryofchange/MakeChange.java
+++ src/libraryofchange/MakeChange.java
@@ -1,13 +1,34 @@
 package libraryofchange;
+
 import java.util.Scanner;
 
 public class MakeChange {
 
 	public static void main(String[] args) {
-		double ;
+		double price = 0, moneytendered = 0, y = 0;
 		Scanner input = new Scanner(System.in);
-		System.out.println("Please enter miles driven: ");
+
+		System.out.println("Please enter the item's price: ");
+		price = input.nextDouble();
+		//input.checkRightPrice();
+		System.out.println("Please enter the amount tendered by customer: ");
+		moneytendered = input.nextDouble();
+		System.out.println("So the amount tendered was ..." + moneytendered);
+		input.close();
 
 	}
 
+	public static boolean isTender(double moneytendered, double price) {
+		boolean result = false;
+		if (moneytendered > price == 0) {
+			result = true;
+		} else {
+			result = false;
+		}
+		return result;
+	}
+	
+
+
 }
+   
